998defde03aa46b0b778a27c31bb0aa73e985748
ABHIJITH T <24mp2275@rit.ac.in>
2025-05-08 23:28:30 +0530

    Create assignment_4.js

---
+++ WEEK-1/DAY_2/assignment_4.js
@@ -0,0 +1,13 @@
+// Write an if/else statement that checks 
+// if a number is even or odd. 
+// If it's even, print "The number is even." 
+// Otherwise, print "The number is odd."
+
+
+let number = 22;
+if(number % 2 == 0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
