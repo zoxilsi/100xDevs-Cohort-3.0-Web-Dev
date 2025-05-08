bde6903cd641bff7425cbeb6b439bdc002326091
ABHIJITH T <24mp2275@rit.ac.in>
2025-05-08 23:23:31 +0530

    Create assignment_2.js

---
+++ WEEK-1/DAY_2/assignment_2.js
@@ -0,0 +1,19 @@
+// Try passing integer
+
+function sum(a, b){
+    return a + b;
+}
+
+let a = 10;
+let b = 20;
+console.log(sum(a, b))
+
+// Try passing String
+
+function sum(x, y){
+    return x + y;
+}
+
+let x = "zoxilsi";
+let y = "Love";
+console.log(sum(x, y));
